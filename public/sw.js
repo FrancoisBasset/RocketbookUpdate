020ccdedfdab98d37ae48df8036d302c452d7afe
FrancoisBasset <>
2020-10-27 20:34:32 +0100

    maj

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,16 @@
+self.addEventListener('install', function(event) {
+	caches.open('cache').then(function(cache) {
+		return cache.addAll([
+			'/index.html',
+			'/images/background_mobile.png',
+			'/images/background_web.png',
+			'/images/icon_192x192.png',
+			'/images/logo.png',
+			'/images/shortcut.png'
+		]);
+	});
+});
+
 self.addEventListener('fetch', event => {
 	event.respondWith(
 		caches.match(event.request).then(response => {
